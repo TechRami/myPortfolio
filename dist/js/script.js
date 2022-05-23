e56cf806766626d5ee24e20aa789820aee92a5ed
techrami <techrami@outlook.com>
2022-05-23 10:15:15 +0300

    fixing navbar issues on mobile 2

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,17 @@
 const menuBtn = document.querySelector('.menu-btn');
+const navbar = document.querySelector('.navbar')
+
+
+
 const animateBtn = () =>{
     menuBtn.classList.toggle('active');
+    
+}
+
+const navbarBgcolor = () =>{
+    navbar.classList.toggle('bgc-dark');
 }
 
 menuBtn.addEventListener('click', animateBtn)
 
-
+navbar.addEventListener('click', navbarBgcolor);
