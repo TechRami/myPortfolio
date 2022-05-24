40a5b2395bd96157717c706a6b7fb1c7f6e3637c
techrami <techrami@outlook.com>
2022-05-24 09:07:45 +0300

    Last touches. restructured all classes and files. changed color and removed the first section animations.

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,9 +3,6 @@ const navbar = document.querySelector('.navbar')
 // const darkModeBtn = document.querySelector('.darkModeBtn')
 const pageBody = document.getElementById('pageBody')
 
-// const darkMode = () => {
-//     pageBody.classList.toggle('darkMode')
-// }
 
 
 const animateBtn = () =>{
@@ -13,9 +10,22 @@ const animateBtn = () =>{
     navbar.classList.toggle('bgc-dark');
 }
 
-
-
 menuBtn.addEventListener('click', animateBtn)
 
 
+//  DARKMODE FUNCTION: Wroking on this later. //
+// const darkMode = () => {
+//     pageBody.classList.toggle('darkMode')
+// }
 // darkModeBtn.addEventListener('click', darkMode);
+
+
+// ----------  Navbar animation on scroll ----------  //
+const navCon = document.querySelector('.navbar');
+window.onscroll = () => {
+    if (window.scrollY > 150) {
+        navCon.classList.add('scrolled');
+    } else {
+        navCon.classList.remove('scrolled');
+    }
+};
